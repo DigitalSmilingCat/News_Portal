bd57c2c92d881588ff9852922b41a3a4e91bea7a
DigitalSmilingCat <avdonin.oleg@gmail.com>
2023-05-26 22:24:33 +0300

    D14

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from .models import *
 
+def nullfy_rating(modeladmin, request, queryset): # все аргументы уже должны быть вам знакомы, самые нужные из них это request — объект хранящий информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(rating=0)
 
-admin.site.register(Author)
+nullfy_rating.short_description = 'Обнулить товары' # описание для более понятного представления в админ панеле задаётся, как будто это объект
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('rating', 'user')
+    list_filter = ('rating', 'user')
+    actions = [nullfy_rating]  # добавляем действия в список
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('type', 'date', 'title', 'rating', 'get_categories')
+    list_filter = ('type',)
+    search_fields = ('title', 'categories__name',)
+
+    @staticmethod
+    def get_categories(obj):
+        return ', '.join(category.name for category in obj.categories.all())
+
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
+
---
+++ NewsPaper/news/management/commands/delete_news.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand, CommandError
+from news.models import Post, Category
+
+
+class Command(BaseCommand):
+    help = 'Удаляет новости из указанной категории.'
+    requires_migrations_checks = True
+
+    def add_arguments(self, parser):
+        parser.add_argument('category', type=str)
+
+    def handle(self, *args, **options):
+        self.stdout.readable()
+        category_name = options['category']
+        self.stdout.write(f"Вы действительно хотите удалить новости из категории {category_name}? y/n")
+        answer = input()
+
+        if answer == 'y':
+            posts = Post.objects.filter(categories__name=category_name)
+            posts.delete()
+            self.stdout.write(self.style.SUCCESS(f"Новости из категории {category_name} удалены!"))
+            return
+
+        self.stdout.write(self.style.ERROR(f"Новости категории {category_name} не тронуты."))
+
+
+
